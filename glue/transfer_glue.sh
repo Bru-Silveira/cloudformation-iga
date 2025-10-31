9fcd48d98bba33b7884753a2a22da55bc2146b0e
Bia Wenzel <biahwenzel@gmail.com>
2025-10-31 16:33:44 -0300

    arquivos .yaml e .sh do Glue finalizados

---
+++ glue/transfer_glue.sh
@@ -0,0 +1,10 @@
+# Para rodar o comando do .sh no cdm, precisa sempre rodar esse comando primeiro:
+SET AWS_ACCESS_KEY_ID=*******
+SET AWS_SECRET_ACCESS_KEY=********
+SET AWS_SESSION_TOKEN=********
+
+# Se quiser validar o template antes de rodar:
+aws cloudformation validate-template --template-body file://trash-glue-setup.yaml 
+
+# depois dele, roda o comando abaixo
+aws cloudformation create-stack --stack-name glue-crawler-and-database-stack --template-body file://trash-glue-setup.yaml --capabilities CAPABILITY_NAMED_IAM --region us-east-1
